1f40799e0d12191c27a3f807b7c47f89edf9a0b7
InaMorais <79170231+InaMorais@users.noreply.github.com>
2021-03-05 12:36:39 -0300

    Add files via upload

---
+++ JogoResilia.py
@@ -0,0 +1,71 @@
+def linha():
+    print ("-" * 50)
+
+linha()
+print ("Conhecendo Rá: a busca na Casa da Vida")
+linha()
+
+
+
+print ("Sinopse")
+
+def texto (txt):
+    print ("-" * 50)
+    print (txt)
+    print ("-" * 50)
+
+texto ("Bela Silva é uma Arqueóloga brasileira que trabalha com antigos encantamentos e reliquias do Egito Antigo. Numa dessas buscas Bela desapareceu misteriosamente. Então seus filhos, perceberam que algumas coisas inexplicáveis, e receberam uma mensagem através de um shabti, que deveriam ir resgatar a Agulha de Cleópatra, na casa da vida, para trazerem sua mãe de volta.")
+
+linha()
+print ("Incio do Jogo")
+linha()
+
+texto ("Juntaram seus pertences e através de um portal, como por encanto chegaram as piramides de Gizé. O shabit novamente deu as instruções de que ele deveriam direcionar o pensamento para que os hieroglifos mágicos o levasem para a porta da casa da vida. Então Mada e Quim deram as mãos e pensaram, e mais uma vez as letras mágicas se formaram no ar e mais uma vez o cenário mudou. Estavam parados em frente a uma porta altíssima com cadeados e correntes enormes, chegaram a Casa da Vida.")
+
+texto ("Mas os Irmãos Silva começaram a achar que tudo estava muito fácil, e pela relíquia que eles deveriam encontrar, não poderia ser tão simples assim. Ouviram um estrondo e após o barulho lá estava Set. ")
+
+texto ("Set é o Deus da morte. ")
+
+texto ("Foi nesse momento que Mada teve uma ideia, ela havia lido no diário perdido de sua mãe uma série de encantamentos, e havia um que servia exatamente para destruir coisas. ")
+
+# decisão 2
+
+texto ("E após os hieroglifos aparecerem as correntes e cadeados explodiram. 1 - Set num contragolpe os manda para o Duat. 2 – Em meio ao caos eles conseguem passar pela porta e entram na casa da vida. ")
+
+# decisão 2
+
+linha()
+print ("No Duat derrota Um")
+linha()
+
+texto ("Quando deram por si estavam em pleno Duat. E imediatamente viram uma horda de demônios correndo na direção deles. Quim disse algumas palavras em egípicio antigo, os hieroglifos pairaram no ar e as 3 primeiras filas de demônios sumiram. Mas de onde vieram aqueles tinha muitos mais. Mada estava mesmo caída lutando mais muitos deles, Quim consegui chegar até ela e lutaram até não mais ter forças e foram vencidos por Set. Era o fim. ")
+
+linha()
+print ("No Duat derrota dois")
+linha()
+
+texto ("Quando deram por si estavam em pleno Duat. E imediatamente viram uma horda de demônios correndo na direção deles. Quim disse algumas palavras em egípcio antigo, os hieroglifos pairaram no ar e todos os demônios sumiram. Set aparece e Quim pronuncia um encantamento que o paralisa por alguns minutos, Quim chama por Mada e eles começam a correr, quando Mada finalmente começou a pronunciar o encantamento uma corda de fogo amarrou nos pés dos irmãos e foram puxados pro Rio dos mortos. Set venceu. Era o fim.  ")
+
+linha()
+print ("No Duat vitoria")
+linha()
+
+texto ("Quando deram por si estavam em pleno Duat. E imediatamente viram uma horda de demônios correndo na direção deles. Quim disse algumas palavras em egípicio antigo, os hieroglifos pairaram no ar e todos os demônios sumiram. Set aparece e Quim pronuncia um encantamento que paralisa Set por alguns minutos, é o tempo de os irmãos saírem correndo desabalados; Enquanto isso Mada procura no diário de sua mãe, um outro encantamento, que os tirasse de lá, Mada pronuncia as palavras e num piscar de olhos voltam para a porta da casa da Vida, que já está com os cadeados e correntes abertos. Se entreolham e decidem seguir pelo corredor. ")
+
+linha()
+print ("Casa da Vida derrota um")
+linha()
+
+texto ("E seguiram pelo corredor, ao final havia uma luz muito brilhante e se dirigiram para lá, porem havia uma armadilha de Rá, para intrusos, e sem querer Mada derrubou o Shabit exatamente em cima de uma pedra que fez o chão sumir. Eles cairam no Lago de fogo. Era o fim. ")
+
+linha()
+print ("Casa da Vida derrota dois")
+linha()
+
+texto ("E seguiram pelo corredor, ao final havia uma luz muito brilhante e se dirigiram para lá, porem Mada achou tudo muito fácil, e mais uma vez consultou o shabit que lhes disse que deveriam tomar cuidado com as armadilhas de Rá. Então Quim pronunciou um encantamento que deveriam revelar onde as armadilhas estavam, porém depois da luta com Set, Quim e Mada estavam enfraquecidos, e já não conseguiam realizá-los como deveriam, então apenas algumas delas se revelaram. Quando estavam quase chegando a porta do Salão do Julgamento, Quim se desequilibrou e caiu, acionando a derradeira armadilha que consistia em escolher entre continuar apenas um deles, enquanto o outro ficaria no lugar de Bela. Ao ouvir isso Mada se atirou sobre o irmão e Rá como castigo transformou-os em pó. Era o fim")
+
+linha()
+print ("Casa da Vida vitoria")
+linha()
+
+texto ("E seguiram pelo corredor, ao final havia uma luz muito brilhante e se dirigiram para lá, porem Mada achou tudo muito fácil, e mais uma vez consultou o shabit que lhes disse que deveriam tomar cuidado com as armadilhas de Rá. Então Quim pronunciou um encantamento que revelava onde as armadilhas estavam. Sabendo disso, desviaram de todas as armadilhas chegaram ao Salão do Julgamento e lá, Rá os esperava, num trono, e os parabenizou pela coragem, e como prêmio entrego-lhes a Agulha da Cleópatra, e emocionados os irmãos Silva, juntos, pronunciaram o encantamento, e, magicamente, Bela surgiu na frente deles. Imediatamente foram abraçar a Mãe e Rá os enviou de volta ao Brasil.")
